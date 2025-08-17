cf2b32dbf74934914fe4bf564ebbf4d2b0bfd7eb
David Ebralidze <dav.ebralidze@gmail.com>
2025-08-17 19:53:34 +0400

    Custom structure

--- src/test/java/MailfenceTest.java
+++ src/test/java/MailfenceTest.java
@@ -9,7 +9,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import java.lang.reflect.Method;
-import java.util.NoSuchElementException;
 
 public class MailfenceTest extends BaseUtility {
 
